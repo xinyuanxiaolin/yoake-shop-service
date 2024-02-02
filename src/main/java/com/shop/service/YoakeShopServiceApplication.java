ee9c1121f285bdafacef5b7a0d26ec78e02071bc
xinyuanxiaolin <xinyuanxuaolin1@163.com>
2024-02-02 17:25:20 +0800

    晨曦手机商城后端初始化

---
+++ src/main/java/com/shop/service/YoakeShopServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shop.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoakeShopServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoakeShopServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shop/service/YoakeShopServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.shop.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoakeShopServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("hello world");
+    }
+
+}
