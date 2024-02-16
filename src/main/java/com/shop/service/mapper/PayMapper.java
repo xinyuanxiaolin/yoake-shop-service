f09494a769fada0f1f72624d0371d18740e98e2b
xinyuanxiaolin <xinyuanxuaolin1@163.com>
2024-02-16 22:16:48 +0800

    在模拟支付完成后扣除相应商品数量

--- src/main/java/com/shop/service/mapper/PayMapper.java
+++ src/main/java/com/shop/service/mapper/PayMapper.java
@@ -1,9 +1,17 @@
 package com.shop.service.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.shop.service.pojo.order.OrderProducts;
 import com.shop.service.pojo.order.Orders;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface PayMapper extends BaseMapper<Orders> {
+
+    @Select("select * from order_products where order_id = #{orderId}")
+    List<OrderProducts> getOrderProducts(String orderId);
+
 }
--- src/main/java/com/shop/service/service/impl/PayServiceImpl.java
+++ src/main/java/com/shop/service/service/impl/PayServiceImpl.java
@@ -2,16 +2,23 @@ package com.shop.service.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.shop.service.common.JwtToken;
+import com.shop.service.mapper.GoodsMapper;
 import com.shop.service.mapper.PayMapper;
+import com.shop.service.pojo.category.CategoryTopItem;
+import com.shop.service.pojo.order.OrderProducts;
 import com.shop.service.pojo.order.Orders;
 import com.shop.service.service.PayService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PayServiceImpl implements PayService {
     @Autowired
     private PayMapper payMapper;
+    @Autowired
+    private GoodsMapper goodsMapper;
     @Autowired
     private JwtToken jwtToken;
 
@@ -22,5 +29,17 @@ public class PayServiceImpl implements PayService {
         UpdateWrapper<Orders> updateWrapper = new UpdateWrapper<>();
         updateWrapper.eq(orderId!=null,"id",orderId).eq("user_id",jwtToken.getUserIdByToken()).set("order_state",2);
         payMapper.update(null,updateWrapper);
+        //对应商品的的库存也要减少
+        //首先先找到对应关联表的goods_id和quantity
+        List<OrderProducts> orderProducts = payMapper.getOrderProducts(orderId);
+        //通过拿到的goods_id去更新category商品表
+
+        orderProducts.forEach(v->{
+            CategoryTopItem categoryTopItem = goodsMapper.selectById(v.getGoodsId());
+            categoryTopItem.setStock(categoryTopItem.getStock()-v.getQuantity());
+            goodsMapper.updateById(categoryTopItem);
+        });
+
+
     }
 }
